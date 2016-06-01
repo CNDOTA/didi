d8054f1cbfee35e4b82cbc9b77c118ae9dd5ab17
rnorm <zhang.nl@outlook.com>
2016-06-01 18:49:38 -0400

    Update run.r

--- run.r
+++ run.r
@@ -67,6 +67,7 @@ params = list(booster='gbtree',
 
 #set.seed(1)
 fit = xgb.train(data=dtrain, nround=10000, watchlist=watchlist,params=params,early.stop.round = 50,maximize = F)
+imp.features=xgb.importance(model=fit,feature_names = vars)
 # train with all data
 dat.train=train.gap[day!=as.Date('2016-01-01') & timeslice>30,]
 dtrain.all=xgb.DMatrix(data=data.matrix(dat.train[,vars,with=F]),label=dat.train$gap,missing=NA)
